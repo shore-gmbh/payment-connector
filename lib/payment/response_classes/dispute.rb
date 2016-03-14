200ac929f772e82bf2b5f8ac09f96d3baf2d3be4
Christoph Schiessl <cs@proactive.cc>
2016-03-14 15:29:34 +0100

    C refactor: define list of Dispute & Evidence attributes only in one place

--- lib/payment/response_classes/dispute.rb
+++ lib/payment/response_classes/dispute.rb
@@ -1,19 +1,25 @@
 module ShorePayment
   # Representation of an {Evidence} object in the Payment Service.
   class Evidence < StripeHash
-    attr_accessor :product_description, :customer_name, :customer_email_address,
-                  :billing_address, :receipt, :customer_signature,
-                  :customer_communication, :uncategorized_file,
-                  :uncategorized_text, :service_date, :service_documentation,
-                  :shipping_address, :shipping_carrier, :shipping_date,
-                  :shipping_documentation, :shipping_tracking_number
+    SUPPORTED_ATTRIBUTES = %i(
+      product_description customer_name customer_email_address billing_address
+      receipt customer_signature customer_communication uncategorized_file
+      uncategorized_text service_date service_documentation shipping_address
+      shipping_carrier shipping_date shipping_documentation
+      shipping_tracking_number
+    ).freeze
+
+    attr_accessor(*SUPPORTED_ATTRIBUTES)
   end
 
   # Representation of a {Dispute} object in the Payment Service.
   class Dispute < StripeHash
-    attr_accessor :id, :status, :reason, :amount_cents, :currency, :created_at,
-                  :organization_id, :due_by, :has_evidence, :past_due,
-                  :submission_count, :evidence
+    SUPPORTED_ATTRIBUTES = %i(
+      id status reason amount_cents currency created_at organization_id due_by
+      has_evidence past_due submission_count evidence
+    ).freeze
+
+    attr_accessor(*SUPPORTED_ATTRIBUTES)
 
     def evidence=(attrs)
       @evidence = Evidence.new(attrs)
